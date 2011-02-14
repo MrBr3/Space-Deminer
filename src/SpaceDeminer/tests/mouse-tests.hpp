3d5998037e942a12436211a833f56c534611535d
Robert Hildebrandt <robert-42@web.de>
2011-02-14 14:53:26 +0100

    Tests: Added Mousetests

--- src/SpaceDeminer/tests/mouse-tests.hpp
+++ src/SpaceDeminer/tests/mouse-tests.hpp
@@ -17,7 +17,165 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-void mouse_tests()
+void mouse_tests(GUITest& gui_test)
 {
   std::cout<<"==== Testing Mouse events ====\n";
+  std::cout<<"---- Testing Bad Mode ----\n";
+  {
+    Framework::Layout layout;
+    Framework::Button btn[8];
+    Framework::Alignment align[3];
+
+    layout.add(btn[0], 8, 8, 32, 32);
+
+    layout.add(align[0],  40, 0, 64, 64);
+    layout.add(align[1], 104, 0, 48, 48);
+    layout.add(btn[3], 200, 0, 128, 128);
+
+    align[0].set(0.5f, 0.5f, 0.f, 0.f);
+    align[1].set(0.5f, 0.5f, 0.f, 0.f);
+
+    align[0].add(btn[1]);
+    align[1].add(btn[2]);
+    btn[1].set_label(":-)");
+    btn[2].set_label("Hihi");
+    btn[3].set_label("Hehe");
+
+    gui_test.add(layout);
+    gui_test.show_all_children();
+    gui_test.compare("mouse-001");
+
+    Framework::Widget::MouseEvent mouse_event;
+
+    mouse_event.x  = 256;
+    mouse_event.y  = 16;
+    main_window->get_window_manager()->on_mouse_move(mouse_event);
+  }
+  std::cout<<"---- Testing Bad Push ----\n";
+  {
+    Framework::Layout layout;
+    Framework::Button btn[8];
+    Framework::Alignment align[3];
+
+    layout.add(btn[0], 8, 8, 32, 32);
+
+    layout.add(align[0],  40, 0, 64, 64);
+    layout.add(align[1], 104, 0, 48, 48);
+    layout.add(btn[3], 200, 0, 128, 128);
+
+    align[0].set(0.5f, 0.5f, 0.f, 0.f);
+    align[1].set(0.5f, 0.5f, 0.f, 0.f);
+
+    align[0].add(btn[1]);
+    align[1].add(btn[2]);
+    btn[1].set_label(":-)");
+    btn[2].set_label("Hihi");
+    btn[3].set_label("Hehe");
+
+    gui_test.add(layout);
+    gui_test.show_all_children();
+
+    {
+      Framework::Widget::MouseEvent mouse_event;
+
+      mouse_event.x  = 256;
+      mouse_event.y  = 16;
+      main_window->get_window_manager()->on_mouse_move(mouse_event);
+    }
+    {
+      Framework::Widget::MouseButtonEvent mouse_event(1, 1);
+
+      mouse_event.x  = 256;
+      mouse_event.y  = 16;
+      main_window->get_window_manager()->on_button_press(mouse_event);
+    }
+  }
+  std::cout<<"---- Testing Move Bad Push ----\n";
+  {
+    Framework::Layout layout;
+    Framework::Button btn[8];
+    Framework::Alignment align[3];
+
+    layout.add(btn[0], 8, 8, 32, 32);
+
+    layout.add(align[0],  40, 0, 64, 64);
+    layout.add(align[1], 104, 0, 48, 48);
+    layout.add(btn[3], 200, 0, 128, 128);
+
+    align[0].set(0.5f, 0.5f, 0.f, 0.f);
+    align[1].set(0.5f, 0.5f, 0.f, 0.f);
+
+    align[0].add(btn[1]);
+    align[1].add(btn[2]);
+    btn[1].set_label(":-)");
+    btn[2].set_label("Hihi");
+    btn[3].set_label("Hehe");
+
+    gui_test.add(layout);
+    gui_test.show_all_children();
+
+    {
+      Framework::Widget::MouseEvent mouse_event;
+
+      mouse_event.x  = 256;
+      mouse_event.y  = 16;
+      main_window->get_window_manager()->on_mouse_move(mouse_event);
+      mouse_event.x  = 256;
+      mouse_event.y  = 15;
+      main_window->get_window_manager()->on_mouse_move(mouse_event);
+    }
+    {
+      Framework::Widget::MouseButtonEvent mouse_event(1, 1);
+
+      mouse_event.x  = 256;
+      mouse_event.y  = 15;
+      main_window->get_window_manager()->on_button_press(mouse_event);
+    }
+  }
+  std::cout<<"---- Testing Move Push Bad Up ----\n";
+  {
+    Framework::Layout layout;
+    Framework::Button btn[8];
+    Framework::Alignment align[3];
+
+    layout.add(btn[0], 8, 8, 32, 32);
+
+    layout.add(align[0],  40, 0, 64, 64);
+    layout.add(align[1], 104, 0, 48, 48);
+    layout.add(btn[3], 200, 0, 128, 128);
+
+    align[0].set(0.5f, 0.5f, 0.f, 0.f);
+    align[1].set(0.5f, 0.5f, 0.f, 0.f);
+
+    align[0].add(btn[1]);
+    align[1].add(btn[2]);
+    btn[1].set_label(":-)");
+    btn[2].set_label("Hihi");
+    btn[3].set_label("Hehe");
+
+    gui_test.add(layout);
+    gui_test.show_all_children();
+
+    {
+      Framework::Widget::MouseEvent mouse_event;
+
+      mouse_event.x  = 256;
+      mouse_event.y  = 16;
+      main_window->get_window_manager()->on_mouse_move(mouse_event);
+      mouse_event.x  = 256;
+      mouse_event.y  = 15;
+      main_window->get_window_manager()->on_mouse_move(mouse_event);
+    }
+    {
+      Framework::Widget::MouseButtonEvent mouse_event(1, 1);
+
+      mouse_event.x  = 256;
+      mouse_event.y  = 15;
+      main_window->get_window_manager()->on_button_press(mouse_event);
+
+      mouse_event.x  = 256;
+      mouse_event.y  = 15;
+      main_window->get_window_manager()->on_button_release(mouse_event);
+    }
+  }
 }
